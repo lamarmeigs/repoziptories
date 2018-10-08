2c26660af7cf368d890ca2e09afe9df5ef620d51
lamarmeigs <lmeigs@shiftgig.com>
2018-10-08 18:08:18 -0500

    Create GithubClient

---
+++ clients/github.py
@@ -0,0 +1,119 @@
+import github
+
+
+class UnknownProfileError(Exception):
+    """Raised when attempting to retrieve information on a non-existent user"""
+    pass
+
+
+class GithubClient:
+    """Provides a basic interface for retrieving relevant data from GitHub"""
+
+    def __init__(self, *args, **kwargs):
+        self.client = github.Github(*args, **kwargs)
+
+    def get_profile(self, profile_name):
+        """Retrieve all relevant data from the named profile.
+
+        Args:
+            profile_name (str): name of the GitHub user whose public data to
+                retrieve (eg. kennethreitz)
+
+        Return:
+            a dict of retrieved API data
+
+        Raise:
+            UnknownProfileError: if the given profile_name does not correspond
+                to GitHub user account
+        """
+        try:
+            user = self.client.get_user(profile_name)
+        except github.UnknownObjectException:
+            raise UnknownProfileError(
+                'No such GitHub account: {}'.format(profile_name)
+            )
+
+        profile = {}
+        profile.update(self._get_user_data(user))
+        profile.update(self._get_repository_data(user))
+        profile.update(self._get_misc_data(user))
+        return profile
+
+    @staticmethod
+    def _get_user_data(user):
+        """Retrieve data related to the given user account
+
+        Args:
+            user (github.NamedUser.NamedUser): an instantiated GitHub user
+
+        Return:
+            a dict containing:
+                - the number of followers
+                - the number of other users followed
+                - the number of stars given
+        """
+        user_data = {
+            'followers': user.followers,
+            'following': user.following,
+            'starred': user.get_starred().totalCount,
+        }
+        return user_data
+
+    @staticmethod
+    def _get_repository_data(user):
+        """Retrieve data related to the given user's repositories.
+
+        Args:
+            user (github.NamedUser.NamedUser): an instantiated GitHub user
+
+        Return:
+            a dict containing:
+                - the number of original repositories
+                - the number of forked repositories
+                - the total number of stars received on all repositories
+                - a list of languages used
+                - a list of topics used
+                - the total number of commits on original repositories
+        """
+        original_repo_count = 0
+        forked_repo_count = 0
+        original_repo_commits = 0
+        stars_received = 0
+        languages = set()
+        topics = set()
+        for repo in user.get_repos():
+            if not repo.forked:
+                original_repo_count += 1
+                original_repo_commits += repo.get_commits(author=user).totalCount
+            else:
+                forked_repo_count += 1
+            stars_received += repo.stargazers_count
+            if repo.language:
+                languages.add(repo.language)
+            if repo.topics:
+                topics = topics.union(set(repo.topics))
+
+        return {
+            'repositories': {
+                'original': original_repo_count,
+                'forked': forked_repo_count,
+            },
+            'stars': stars_received,
+            'languages': list(languages),
+            'topics': list(topics),
+        }
+
+    def _get_misc_data(self, user):
+        """Retrieve relevant non-user, non-repository profile data.
+
+        Args:
+            user (github.NamedUser.NamedUser): an instantiated GitHub user
+
+        Return:
+            a dict containing:
+                - the number of issues created by the user
+        """
+        # TODO: Calculate the "size" of the user account
+        return {
+            'issues': self.client.search_issues('author:{}'.format(user.login)).totalCount
+        }
---
+++ tests/clients/test_github.py
@@ -0,0 +1,97 @@
+from contextlib import ExitStack
+from unittest import mock, TestCase
+
+import github
+
+from clients.github import GithubClient, UnknownProfileError
+
+
+class UnknownProfileErrorTestCase(TestCase):
+    def test_inheritance(self):
+        self.assertIsInstance(UnknownProfileError(), Exception)
+
+
+class GithubClientTestCase(TestCase):
+    def test_init_creates_client(self):
+        client = GithubClient()
+        self.assertTrue(hasattr(client, 'client'))
+        self.assertIsInstance(client.client, github.Github)
+
+    def test_get_profile_raises_error_on_unknown_user(self):
+        client = GithubClient()
+        error = github.UnknownObjectException('test error', None)
+        with mock.patch.object(client.client, 'get_user', side_effect=error):
+            with self.assertRaises(UnknownProfileError) as cm:
+                client.get_profile('foobar')
+
+        self.assertEqual(str(cm.exception), 'No such GitHub account: foobar')
+
+    def test_get_profile_assembles_profile_data(self):
+        client = GithubClient()
+        with ExitStack() as stack:
+            context_managers = (
+                mock.patch.object(client.client, 'get_user'),
+                mock.patch.object(client, '_get_user_data', return_value={'user': 'data'}),
+                mock.patch.object(client, '_get_repository_data', return_value={'repo': 'data'}),
+                mock.patch.object(client, '_get_misc_data', return_value={'misc': 'data'}),
+            )
+            for context_manager in context_managers:
+                stack.enter_context(context_manager)
+
+            data = client.get_profile('foobar')
+            client._get_user_data.assert_called_once_with(
+                client.client.get_user.return_value
+            )
+            client._get_repository_data.assert_called_once_with(
+                client.client.get_user.return_value
+            )
+            client._get_misc_data.assert_called_once_with(
+                client.client.get_user.return_value
+            )
+            self.assertEqual(
+                data,
+                {'user': 'data', 'repo': 'data', 'misc': 'data'}
+            )
+
+    def test_get_user_data_retrieves_data(self):
+        user = mock.MagicMock()
+        user.followers = 5
+        user.following = 2
+        user.get_starred.return_value.totalCount = 8
+
+        data = GithubClient._get_user_data(user)
+        self.assertEqual(data, {'followers': 5, 'following': 2, 'starred': 8})
+
+    def test_get_repository_data_retrieves_data(self):
+        user = mock.MagicMock()
+        user.get_repos.return_value = [
+            mock.MagicMock(
+                forked=True,
+                stargazers_count=2,
+                language='Python',
+                topics=['test', 'repositories']
+            ),
+            mock.MagicMock(
+                forked=False,
+                stargazers_count=3,
+                language='JavaScript',
+                topics=None,
+            )
+        ]
+
+        data = GithubClient._get_repository_data(user)
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['repositories'], {'original': 1, 'forked': 1})
+        self.assertEqual(data['stars'], 5)
+        self.assertCountEqual(data['languages'], ['Python', 'JavaScript'])
+        self.assertCountEqual(data['topics'], ['test', 'repositories'])
+
+    def test_get_misc_data_retrieves_data(self):
+        client = GithubClient()
+        user = mock.MagicMock(login='foobar')
+        with mock.patch.object(client.client, 'search_issues') as mock_search:
+            mock_search.return_value.totalCount = 4
+            data = client._get_misc_data(user)
+
+        mock_search.assert_called_once_with('author:foobar')
+        self.assertEqual(data, {'issues': 4})
