2499bf18129d9bdcd74f6592a295de26a6062d57
lamarmeigs <lmeigs@shiftgig.com>
2018-10-08 20:53:34 -0500

    Create general-purpose exception class

--- clients/exceptions.py
+++ clients/exceptions.py
@@ -1,8 +1,13 @@
-class UnknownProfileError(Exception):
+class ApiResponseError(Exception):
+    """Raised when the 3rd-party API has returned an unexpected error"""
+    pass
+
+
+class UnknownProfileError(ApiResponseError):
     """Raised when attempting to retrieve information on a non-existent user"""
     pass
 
 
-class RateLimitError(Exception):
+class RateLimitError(ApiResponseError):
     """Raised when the 3rd-party service's rate limit has been hit"""
     pass
--- tests/clients/test_exceptions.py
+++ tests/clients/test_exceptions.py
@@ -3,11 +3,22 @@ from unittest import TestCase
 from clients import exceptions
 
 
+class ApiResponseErrorTestcase(TestCase):
+    def test_inheritance(self):
+        self.assertIsInstance(exceptions.ApiResponseError(), Exception)
+
+
 class UnknownProfileErrorTestCase(TestCase):
     def test_inheritance(self):
-        self.assertIsInstance(exceptions.UnknownProfileError(), Exception)
+        self.assertIsInstance(
+            exceptions.UnknownProfileError(),
+            exceptions.ApiResponseError
+        )
 
 
 class RateLimitErrorTestCase(TestCase):
     def test_inheritance(self):
-        self.assertIsInstance(exceptions.RateLimitError(), Exception)
+        self.assertIsInstance(
+            exceptions.RateLimitError(),
+            exceptions.ApiResponseError
+        )
